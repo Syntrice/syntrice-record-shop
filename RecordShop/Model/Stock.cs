712f4b36c6728a31915b21d2bdb432637f2c0c5b
Samuel M <76839403+Syntrice@users.noreply.github.com>
2025-01-14 11:28:52 +0000

    feat: Setup basic model for managing record stocks
    Setup a basic EF Core code-first model by adding three DbSets to
    RecordShopDbContext: Records, Stocks, and Genres. This is quite rudamentry for
    now, as ties down the application into only handling one product type (Records).

---
+++ RecordShop/Model/Stock.cs
@@ -0,0 +1,10 @@
+﻿namespace RecordShop.Model
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public Record Record { get; set; } = null!;
+        public int Quantity { get; set; }
+        public int UnitPrice { get; set; }
+    }
+}
