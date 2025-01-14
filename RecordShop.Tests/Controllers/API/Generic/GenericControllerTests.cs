e1068ca086da519aac8897fa114c965c93e7bdab
Samuel M <76839403+Syntrice@users.noreply.github.com>
2025-01-14 21:34:03 +0000

    feat: Scaffold GenericController class
    Create GenericController and corresponding test class. Scaffold method
    signitures. This will be used to implement common CRUD endpoints, which can be
    implemented by extending this class in other controllers.

---
+++ RecordShop.Tests/Controllers/API/Generic/GenericControllerTests.cs
@@ -0,0 +1,35 @@
+﻿namespace RecordShop.Tests.Controllers.API.Generic
+{
+    public class GenericControllerTests
+    {
+        [Test()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetByIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void PostTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void PutTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ RecordShop/Controllers/API/Generic/GenericController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecordShop.Model;
+using RecordShop.Services.Generic;
+
+namespace RecordShop.Controllers.API.Generic
+{
+    public class GenericController<TEntity> : ControllerBase where TEntity : class, IEntity
+    {
+        private readonly IGenericService<TEntity> _genericService;
+
+        public GenericController(IGenericService<TEntity> genericService)
+        {
+            _genericService = genericService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return BadRequest();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return BadRequest();
+        }
+
+        [HttpPost]
+        public IActionResult Post(Genre genre)
+        {
+            return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return BadRequest();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Genre genre)
+        {
+            return BadRequest();
+        }
+    }
+}
