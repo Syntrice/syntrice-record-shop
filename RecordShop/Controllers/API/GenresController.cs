bdd91fd66e66a357ead07f033eaeef3ffc68aca1
Samuel M <76839403+Syntrice@users.noreply.github.com>
2025-01-14 13:21:30 +0000

    feat: Add GenresController and RecordsController

---
+++ RecordShop/Controllers/API/GenresController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecordShop.Controllers.API
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GenresController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Hello()
+        {
+            return Ok("Hello world!");
+        }
+    }
+}
---
+++ RecordShop/Controllers/API/RecordsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecordShop.Controllers.API
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RecordsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Hello()
+        {
+            return Ok("Hello world!");
+        }
+    }
+}
