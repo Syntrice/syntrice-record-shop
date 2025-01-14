4a7d779a76671fed4efd5802951125623ea96181
Samuel M <76839403+Syntrice@users.noreply.github.com>
2025-01-14 10:24:07 +0000

    feat: Setup DbContext configuration
    Add a WebApplicationBuilder extension method to configure the
    RecordShopDbContext. It reads a boolean configuration value
    "UseInMemoryDatabase" to check whether to use the Entity Framework In Memory
    Provider. If this is set to false, it will instead look for a connection
    string to setup a Sql Server connection.

---
+++ RecordShop/Model/RecordShopDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RecordShop.Model
+{
+    public class RecordShopDbContext : DbContext
+    {
+        public RecordShopDbContext(DbContextOptions<RecordShopDbContext> options) : base(options) { }
+    }
+}
