ebfc0680e1140ed13a71df8013906a0b70893057
Samuel M <76839403+Syntrice@users.noreply.github.com>
2025-01-14 15:31:38 +0000

    feat: Add GenericService classes
    These classes are used to create simple services used to handle simple CRUD
    logic.

---
+++ RecordShop/Services/Generic/GenericService.cs
@@ -0,0 +1,41 @@
+﻿using RecordShop.Model;
+using RecordShop.Repositories.Generic;
+using RecordShop.Services.Response;
+
+namespace RecordShop.Services.Generic
+{
+    public class GenericService<TEntity> : IGenericService<TEntity> where TEntity : class, IEntity
+    {
+        private readonly IGenericRepository<TEntity> _repository;
+
+        public GenericService(IGenericRepository<TEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        public ServiceResponse DeleteEntityById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceObjectResponse<List<TEntity>> GetEntities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceObjectResponse<TEntity> GetEntityById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceObjectResponse<TEntity> InsertEntity(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceResponse UpdateEntity(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RecordShop/Services/Generic/IGenericService.cs
@@ -0,0 +1,22 @@
+﻿using RecordShop.Model;
+using RecordShop.Services.Response;
+
+namespace RecordShop.Services.Generic
+{
+    public interface IGenericService<TEntity> where TEntity : class, IEntity
+    {
+        // Create
+        public ServiceObjectResponse<TEntity> InsertEntity(TEntity entity);
+
+        // Read
+        public ServiceObjectResponse<List<TEntity>> GetEntities();
+
+        public ServiceObjectResponse<TEntity> GetEntityById(int id);
+
+        // Update
+        public ServiceResponse UpdateEntity(TEntity entity);
+
+        // Delete
+        public ServiceResponse DeleteEntityById(int id);
+    }
+}
