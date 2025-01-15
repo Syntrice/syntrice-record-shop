738907790327da9e802c983acf0ac544a8974159
Samuel M <76839403+Syntrice@users.noreply.github.com>
2025-01-15 16:48:48 +0000

    feat: Add ArtistsController for basic CRUD operations

---
+++ RecordShop/Controllers/API/ArtistsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecordShop.Controllers.API.Generic;
+using RecordShop.Model.ArtistModel;
+using RecordShop.Services;
+using RecordShop.Services.Generic;
+
+namespace RecordShop.Controllers.API
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ArtistsController : GenericMappingController<Artist, ArtistGetDTO, ArtistInsertDTO, ArtistUpdateDTO>
+    {
+        public ArtistsController(IArtistsService service) : base(service)
+        {
+        }
+    }
+}
